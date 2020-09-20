326af3c509a1b6195c8481984599915ef415d957
NathanCheshire <nathan.vincent.2.718@gmail.com>
2020-09-20 00:32:53 -0500

    Saturday Night done, fuck Java

--- src/AStarPathFinding.java
+++ src/AStarPathFinding.java
@@ -38,102 +38,18 @@ public class AStarPathFinding extends PathFinder {
         graph[controlGhost.getExactX()][controlGhost.getExactY()] = controlGhost;
     }
 
-//    class NodePriorityQueue extends PriorityQueue {
-//        public boolean contains(Node node) {
-//            System.out.println("inside of contains");
-//            Iterator<Node> iterator = this.iterator();
-//
-//            while (iterator.hasNext()) {
-//                Node testNode = iterator.next();
-//
-//                if (testNode.getNodeX() == node.getNodeX() && testNode.getNodeY() == node.getNodeY())
-//                    return true;
-//            }
-//
-//            return false;
-//        }
-//    }
-
-    public class NodePriorityQueue extends PriorityQueue<Node> {
-        @Override
-        public boolean contains(Object obj) {
-            if (!(obj instanceof Node)) {
-                return false;
-            }
-
-            Node node = (Node) obj;
-
-            System.out.println("inside of contains");
-
-            for (Node testNode : this) {
-                if (testNode.getNodeX() == node.getNodeX() && testNode.getNodeY() == node.getNodeY())
-                    return true;
-            }
-
-            return false;
-        }
-    }
+    //todo idea: use an integer combo (x,y) and use priority queue in that way instead of using a priority queue on nodes?
 
     @Override
     public void resfreshPath(Node[][] graph) {
         try {
-            NodePriorityQueue pq = new NodePriorityQueue();
-
             int ghostX = controlGhost.getExactX();
             int ghostY = controlGhost.getExactY();
 
             int pacX = pac.getExactX();
             int pacY = pac.getExactY();
 
-            //todo use localGraph and conduct A* on, also remove the old path and draw the new one if drawpaths is enabled for the main grid
-
-            Node startNode = graph[ghostX][ghostY];
-            startNode.setgCost(0);
-            startNode.setNodeParent(null);
-            pq.add(startNode);
-
-
-            //while priority queue is not empty
-            while (!pq.isEmpty()) {
-                Node minNode = pq.poll();
-
-                if (minNode.getNodeX() == pacX && minNode.getNodeY() == pacY) {
-                    //make a step depending on the parent of Node[controlGhost.getExactX()][controlGhost.getExactY()]
-                    System.out.println("Path found");
-                }
-
-                for (int x = minNode.getNodeX() - 1 ; x < minNode.getNodeX() + 2; x++) {
-                    for (int y = minNode.getNodeY() - 1 ; y < minNode.getNodeY() + 2 ; y++) {
-                        if (x == minNode.getNodeX() && minNode.getNodeY() == y)
-                            continue;
-
-                        if (x > 0 && y > 0 && x < 40 && y < 40) {
-                            //is a neighbor here
-                            if (!pq.contains(graph[x][y])) {
-
-//                                double g = minNode.getGCost() + Math.sqrt(Math.pow((graph[x][y].getNodeX() - minNode.getNodeX()), 2) +
-//                                                   Math.pow((graph[x][y].getNodeY() - minNode.getNodeY()), 2));
-//                                graph[x][y].setgCost(g);
-//                                graph[x][y].setNodeParent(minNode);
-//
-                                graph[x][y] = new Node(x,y);
-                                graph[x][y].setNodeX(x);
-                                graph[x][y].setNodeY(y);
-                                System.out.println("adding: " + graph[x][y]);
-                                pq.add(graph[x][y]);
-                            }
-
-//                            else if (minNode.getGCost() + Math.sqrt(Math.pow((graph[x][y].getNodeX() - minNode.getNodeX()), 2) +
-//                                    Math.pow((graph[x][y].getNodeY() - minNode.getNodeY()), 2)) < graph[x][y].getGCost()) {
-//
-//                                graph[x][y].setgCost(minNode.getGCost() + Math.sqrt(Math.pow((graph[x][y].getNodeX() - minNode.getNodeX()), 2) +
-//                                        Math.pow((graph[x][y].getNodeY() - minNode.getNodeY()), 2)));
-//                                graph[x][y].setNodeParent(minNode);
-//                            }
-                        }
-                    }
-                }
-            }
+            //path find from (ghostX, ghostY) to (pacX, pacY). If possible, take a step in the best direction
 
             System.out.println("Path DNE");
         }
@@ -148,6 +64,4 @@ public class AStarPathFinding extends PathFinder {
         this.pac = pac;
         this.controlGhost = controlGhost;
     }
-
-    //todo redo getNeighbors in other algorithms
 }
--- src/Controller.java
+++ src/Controller.java
@@ -18,7 +18,6 @@ import javafx.scene.shape.Line;
 
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class Controller {
